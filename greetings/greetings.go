8eba9508ae5aee099cac2bf4ad8f5ecb559389c2
msd <87383399+msd@users.noreply.github.com>
2022-08-14 16:37:58 +0100

    Multiple people greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -19,7 +19,29 @@ func Hello(name string) (string, error) {
     return message, nil
 }
 
+// HelloMany returns a map that associates each of the named
+// people with a greeting message
+func HelloMany(names []string) (map[string]string, error) {
+    // A map to associate names with messages.
+    messages := make(map[string]string)
+    // Loop through the received slice of names, calling
+    // the Hello function to get a message for each name.
+    for _, name := range names {
+        message, err := Hello(name)
+        {
+            if err != nil {
+                return nil, err             
+            }
+        // In the map, associate the retrieved message with
+        // the name.
+            messages[name] = message
+        }
+    }
+    return messages, nil
+}
+
 // init sets initial values for variables used in the function.
+// Called automatically when this module is loaded.
 func init() {
     rand.Seed(time.Now().UnixNano())
 }
--- hello/hello.go
+++ hello/hello.go
@@ -15,15 +15,18 @@ func main() {
     log.SetFlags(0)
 
 
-    // Request a greeting message.
-    message, err := greetings.Hello("Steve")
+    // A slice of names.
+    names := []string{"Steve", "Stevie", "Stevia"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.HelloMany(names)
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
         log.Fatal(err)
     }
 
-    // If no error was returned, print the returned message
-    // to the console.
-	fmt.Println(message)
+    // If no error was returned, print the returned map of
+    // messages to the console.
+	fmt.Println(messages)
 }
